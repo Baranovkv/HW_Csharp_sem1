3090a2042166311845405b1a5328364972ca6241
Kirill <k-baranov@outlook.com>
2023-01-11 11:34:32 -0800

    variables max and min removed

--- Program.cs
+++ Program.cs
@@ -4,14 +4,10 @@ Console.WriteLine("Enter the second whole number, please");
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 > number2)
 {
-    int max = number1;
-    int min = number2;
-    Console.WriteLine($"Maximum = {max}, Minimum = {min}");
+    Console.WriteLine($"Maximum = {number1}, Minimum = {number2}");
 }
 else if (number1 < number2) 
 {
-    int max = number2;
-    int min = number1;
-    Console.WriteLine($"Maximum = {max}, Minimum = {min}");
+    Console.WriteLine($"Maximum = {number2}, Minimum = {number1}");
 }
 else Console.WriteLine("Numbers are equals");
