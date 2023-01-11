3eaf029c2ff424c34bbff63eec1f86642ad4019a
Kirill <k-baranov@outlook.com>
2023-01-11 11:24:13 -0800

    Firt task completed

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter the first whole number, please");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second whole number, please");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    int max = number1;
+    int min = number2;
+    Console.WriteLine($"Maximum = {max}, Minimum = {min}");
+}
+else if (number1 < number2) 
+{
+    int max = number2;
+    int min = number1;
+    Console.WriteLine($"Maximum = {max}, Minimum = {min}");
+}
+else Console.WriteLine("Numbers are equals");
