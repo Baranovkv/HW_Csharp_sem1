459a595a250c026c2efd34af5fca7fddcc354279
Kirill <k-baranov@outlook.com>
2023-01-11 12:02:06 -0800

    task 6 complited

---
+++ Task6/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Enter an integer, please");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2 > 0) Console.WriteLine("The nuber is odd"); // если отстаток от деления больше нуля, то выводим, что число нечетное
+else Console.WriteLine("The number is even"); // иначе выводим, что число четное
